6f06c8afb98bb18c60abfff99ee74d3ff6765eff
Percylikezalmendz <percyatlife@gmail.com>
2024-05-23 21:05:49 -0700

    Update lab.js
    color

--- projectttt/lab.js
+++ projectttt/lab.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Add a click event listener to each "name" element
         name.addEventListener("click", function() {
             // Change the background color of the clicked "name" element
-            this.style.backgroundColor = "lightblue";
+            this.style.backgroundColor = "rgb(254, 252, 184";
 
             // You can add more actions or functionality here
         });
